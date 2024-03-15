951416723810acbaeab47f302ce22c23ba5a11b5
Midokh987 <116901798+Midokh987@users.noreply.github.com>
2024-03-15 06:51:49 +0200

    Update grad.go

---
+++ grad.go
@@ -0,0 +1,33 @@
+package main
+
+import "time"
+
+type MessageRequest struct {
+	CreatedAt time.Time `json:"createdAt"`
+	TEXT      string    `json:"Msg"`
+	useremail string    `json:"email"`
+}
+type MessageResponse struct {
+	CreatedAt time.Time `json:"createdAt"`
+	TEXT      string    `json:"Msg"`
+	useremail string    `json:"email"`
+}
+
+func createMsgTable() error {
+	query := `
+	CREATE TABLE Msg (
+		CreatedAt timestamp NOT NULL,
+		text text NOT NULL,
+		useremail text NOT NULL,
+		msgID serial PRIMARY KEY
+	);
+    `
+	err := s.db.Exec(query)
+	return err
+	CloseDatabase()
+}
+func CloseDatabase() error {
+	return db.Close()
+}
+
+//start.go
